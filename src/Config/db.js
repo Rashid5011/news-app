f589a3151e5757789788a9213d2610b8a1bf09d3
Rashid5011 <106409500+Rashid5011@users.noreply.github.com>
2023-12-20 17:25:36 +0500

    Update db.js

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
 
-const connectDB = () => {
-  mongoose.connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    bufferCommands: false, // Disable mongoose buffering
-    bufferMaxEntries: 0, // and MongoDB driver buffering
-    serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of 30 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  })
-  .then(() => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0, // and MongoDB driver buffering
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of 30 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    });
     console.log(`Server Connected ${mongoose.connection.host}`.cyan.bold);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(`Server Not Connected ${error.message}`.red.underline.bold);
-  });
-}
+  }
+};
 
 export default connectDB;
